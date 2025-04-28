50eef438fe0cf1499a2ff7e5ed16d9c05d566699
Tuyen Dang <dptuyenn1@gmail.com>
2025-04-28 16:58:23 +0700

    chore: remove unused throws exception

--- demo/src/main/java/com/dev/demo/controllers/AuthController.java
+++ demo/src/main/java/com/dev/demo/controllers/AuthController.java
@@ -5,7 +5,6 @@ import com.dev.demo.dto.request.RegisterRequest;
 import com.dev.demo.dto.response.AuthResponse;
 import com.dev.demo.dto.response.UserResponse;
 import com.dev.demo.services.AuthService;
-import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +20,7 @@ public class AuthController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public AuthResponse login(@RequestBody LoginRequest request) throws JOSEException {
+    public AuthResponse login(@RequestBody LoginRequest request) {
         AuthResponse authResponse = new AuthResponse();
 
         authResponse.setAccessToken(authService.login(request));
--- demo/src/main/java/com/dev/demo/services/AuthService.java
+++ demo/src/main/java/com/dev/demo/services/AuthService.java
@@ -3,11 +3,10 @@ package com.dev.demo.services;
 import com.dev.demo.dto.request.LoginRequest;
 import com.dev.demo.dto.request.RegisterRequest;
 import com.dev.demo.dto.response.UserResponse;
-import com.nimbusds.jose.JOSEException;
 
 public interface AuthService {
 
-    String login(LoginRequest request) throws JOSEException;
+    String login(LoginRequest request);
 
     UserResponse me(String username);
 
