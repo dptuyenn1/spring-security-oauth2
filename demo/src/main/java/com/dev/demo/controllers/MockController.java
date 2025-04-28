5f232133bdb536943670dee341ba60953a5d7bc4
Tuyen Dang <dptuyenn1@gmail.com>
2025-04-28 12:25:01 +0700

    feat (mock api): add resource to test role-based authentication

---
+++ demo/src/main/java/com/dev/demo/controllers/MockController.java
@@ -0,0 +1,16 @@
+package com.dev.demo.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mock")
+public class MockController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from protected endpoint!";
+    }
+}
