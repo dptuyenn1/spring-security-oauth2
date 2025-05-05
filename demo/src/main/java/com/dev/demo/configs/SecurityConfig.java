ed73f3fff1e3efc3d929065c9107fe9100d9e116
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-05 19:17:55 +0700

    chore (security): remove unused config

--- demo/src/main/java/com/dev/demo/configs/SecurityConfig.java
+++ demo/src/main/java/com/dev/demo/configs/SecurityConfig.java
@@ -36,7 +36,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
