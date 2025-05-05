50f16570b08c9ca7cb02f419becc987ec34fb19a
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-05 19:18:41 +0700

    chore (dto): add serial version uid for user response

--- demo/src/main/java/com/dev/demo/dto/response/UserResponse.java
+++ demo/src/main/java/com/dev/demo/dto/response/UserResponse.java
@@ -4,11 +4,15 @@ import com.dev.demo.dto.BaseDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.util.Set;
 
 @Getter
 @Setter
 public class UserResponse extends BaseDTO {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private String firstName;
     private String lastName;
