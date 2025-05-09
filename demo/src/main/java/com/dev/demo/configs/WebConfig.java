f116c5d013b9acececcf40f6f3c4f82f8472becb
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-09 12:16:46 +0700

    chore: use constants instead of direct value

--- demo/src/main/java/com/dev/demo/configs/WebConfig.java
+++ demo/src/main/java/com/dev/demo/configs/WebConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String PATH_PREFIX =
-            String.format("%s/%s", Constants.OTHERS.API_PREFIX, Constants.OTHERS.API_VERSION);
+    private static final String PATH_PREFIX = String.format(Constants.API.PATH_PREFIX_FORMAT,
+            Constants.API.PREFIX, Constants.API.VERSION);
     private static final String BASE_PACKAGE = "com.dev.demo.controllers";
 
     @Override
--- demo/src/main/java/com/dev/demo/helpers/Constants.java
+++ demo/src/main/java/com/dev/demo/helpers/Constants.java
@@ -30,9 +30,10 @@ public class Constants {
         public static final String SUCCESS = "{0} successfully";
     }
 
-    public static class OTHERS {
+    public static class API {
 
-        public static final String API_PREFIX = "/api";
-        public static final String API_VERSION = "v1";
+        public static final String PATH_PREFIX_FORMAT = "/%s/%s";
+        public static final String PREFIX = "api";
+        public static final String VERSION = "v1";
     }
 }
