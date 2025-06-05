da18b371cea80a1ead6897091c6903c2a6d8c242
Tuyen Dang <dptuyenn1@gmail.com>
2025-06-05 07:57:29 +0700

    feat (audit config): correct bean

--- demo/src/main/java/com/dev/configs/JpaAuditConfig.java
+++ demo/src/main/java/com/dev/configs/JpaAuditConfig.java
@@ -2,21 +2,17 @@ package com.dev.configs;
 
 import com.dev.configs.impl.AuditAwareImpl;
 import com.dev.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableJpaAuditing
 public class JpaAuditConfig {
 
-    private final UserRepository userRepository;
-
     @Bean
-    public AuditorAware<String> auditorProvider() {
+    public AuditorAware<String> auditorProvider(UserRepository userRepository) {
         return new AuditAwareImpl(userRepository);
     }
 }
