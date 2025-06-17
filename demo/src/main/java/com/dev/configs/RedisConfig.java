0f8b0a28ed2b908d02b12622d9f0b8a5a5bf0a82
Tuyen Dang <dptuyenn1@gmail.com>
2025-06-17 10:43:46 +0700

    chore (redis): config serializer

--- demo/src/main/java/com/dev/configs/RedisConfig.java
+++ demo/src/main/java/com/dev/configs/RedisConfig.java
@@ -4,15 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<?, ?> template = new RedisTemplate<>();
+    public RedisTemplate<String, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ?> template = new RedisTemplate<>();
 
         template.setConnectionFactory(connectionFactory);
+
+        //without these lines, the records will be saved by binary format
+        //use for ValueOperations
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        //use for HashOperations
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return template;
     }
